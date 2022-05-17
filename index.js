9fbc3d7446359350f0db79c2f04c1d57d4469583
Joon Jung <joonjung55@gmail.com>
2022-05-16 23:52:39 -0400

    commit tree 1e446a0f94a51b2ba986822e5a3785d31d2c67b7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(act='roller-skate'){
+    return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(act1='go to the office'){
+    return `This Monday, I will ${act1}.`
+}
+function wrapAdjective(visualFlair = '*'){
+    const innerfunction = function(adjective = 'special'){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+    return innerfunction
+}
+
+wrapAdjective('~')('a goddamn genius')
